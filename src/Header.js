68b75fb34f8a5f9fb7ee1d2d852d625ca9938ee1
thestriver <wannabeamumin@gmail.com>
2020-06-03 23:21:58 +0100

    edits

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(){
+    return (
+        <div className="bg-teal-500 "> 
+            <img src={"/images/bg-header-desktop.svg"} />
+        </div>
+    )
+}
+
+
+export default Header;
