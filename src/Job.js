8d6a93854bcd4f0773c3632f647520714d50ab30
thestriver <wannabeamumin@gmail.com>
2020-06-08 17:47:10 +0100

    ternary expression

--- src/Job.js
+++ src/Job.js
@@ -7,7 +7,10 @@ function Job(props){
             {
                 props.items.map(item => {
                     return <div key={item.id} className="max-w-full mx-32 my-6 py-6 bg-white rounded-lg shadow-lg flex">
-                                <div class="bg-teal-600 rounded-lg w-1 h-auto -mt-6 -mb-6"></div>
+                                {
+                                    item.featured ? <div class="bg-teal-600 rounded-lg w-1 h-auto -mt-6 -mb-6"></div> : null
+                                }
+                                
                                 <div className="flex lg:w-6/12">
                                     <div className="ml-6 mr-5">
                                         <img class="h-24 w-24 rounded-full" src={item.logo} alt="" />
@@ -16,10 +19,12 @@ function Job(props){
                                         <div className="text-lg inline-flex leading-8 font-bold text-teal-500 pr-3">{item.company}</div>
                                         <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-teal-400 text-green-800 mr-2">
                             {
-                             item.new
+                             item.new  ? "NEW" : null
                             }</span>
                                         <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-black text-white">
-                            {item.featured}</span>
+                            {
+                            item.featured ? "FEATURED" : null
+                            }</span>
 
                                         <div className="text-blue-900 text-xl">{item.position}</div>
                                         <span className="text-gray-600 font-light mr-3">{item.postedAt}</span>
